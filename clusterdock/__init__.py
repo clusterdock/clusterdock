92f2f31385253e4427b78fa85b560a281faf649a
Dima Spivak <dima@spivak.ch>
2017-09-02 16:31:40 -0700

    Bump version: 1.0.2 → 1.0.3

--- clusterdock/__init__.py
+++ clusterdock/__init__.py
@@ -17,7 +17,7 @@ import logging
 
 __author__ = """Dima Spivak"""
 __email__ = 'dima@spivak.ch'
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter('%(asctime)s %(name)-20s %(levelname)-8s %(message)s',
