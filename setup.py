1fbc1f85546f1afdd024eef5b46b7ecbddbabec5
Dima Spivak <dima@spivak.ch>
2017-10-23 14:37:02 -0700

    Bump version: 1.1.0 → 1.2.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ test_requirements = [
 
 setup(
     name='clusterdock',
-    version='1.1.0',
+    version='1.2.0',
     description="clusterdock is a framework for creating Docker-based container clusters",
     long_description=readme + '\n\n' + history,
     author="Dima Spivak",
